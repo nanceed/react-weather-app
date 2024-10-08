1b94f3ffc0d74675a80e9da804b5879b8ea74d0f
Nancy <duong.nancy@gmail.com>
2024-10-07 22:09:00 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
+  );
+}
