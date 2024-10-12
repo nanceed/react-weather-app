d99a2b679637b6e4cac3f713bc4108797fb5f348
Nancy <duong.nancy@gmail.com>
2024-10-11 18:24:29 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import WeatherSearch from "./WeatherSearch";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
+      
       <WeatherSearch />
       <footer>
         This project was coded by Nancy Duong and is open-sourced on
         <a href="https://github.com/nanceed/react-weather-app" target="_blank"> GitHub </a>
         </footer>
+        </div>
     </div>
   );
 }
