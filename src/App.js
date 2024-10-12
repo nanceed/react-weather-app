9ad98aa2926e46c52200696d70f0b5a34d0d7a32
Nancy <duong.nancy@gmail.com>
2024-10-12 01:01:49 -0700

    Added API integration

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-       <Weather /> 
+       <Weather defaultCity="New York"/> 
       <footer>
         This project was coded by Nancy Duong and is open-sourced on
         <a href="https://github.com/nanceed/react-weather-app" target="_blank"> GitHub </a>
