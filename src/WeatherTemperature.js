790b2d9a9ada6281a679c01c986c6fc4c2669ced
Nancy <duong.nancy@gmail.com>
2024-10-14 20:08:46 -0700

    Fixed the icon descriptions

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+    const [unit, setUnit] = useState("celsius");
+    if (unit === "celsius") {
+        return (
+            <div className="WeatherTemperature">
+                <span className="temperature">{Math.round(props.celsius)}</span>
+                <span className="unit">°C</span>
+            </div>
+        );
+    } else {
+        return "F"
+    }
+
+    }
