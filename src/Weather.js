50e62c245489fc2e0a0bdbc00b76ae53772e490e
Nancy <duong.nancy@gmail.com>
2024-10-16 14:49:11 -0700

    Added weather forecast day to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -9,9 +9,10 @@ export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false });
     const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
-        console.log(response.data);
+        
         setWeatherData({
             ready: true,
+            coordinates: response.data.coordinates,
             temperature: response.data.temperature.current,
             humidity: response.data.temperature.humidity,
             date: new Date(response.data.time * 1000),
@@ -59,7 +60,7 @@ export default function Weather(props) {
                     </div>
                 </form>
                 <WeatherInfo data={weatherData} />
-                <WeatherForecast />
+                <WeatherForecast coordinates={weatherData.coordinates} />
             </div>
         );
     } else {
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,20 +1,43 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
-export default function WeatherForecast() {
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                  <div className="WeatherForecast-day">Thu</div>  
-                    <WeatherIcon code="clear-sky-day" size={36} />
-                    <div className="WeatherForecast-temperatures">
-                        <span className="WeatherForecast-temperature-max">19°</span>
-                        <span className="WeatherForecast-temperature-min">10°</span>             
-                    </div>
+export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+    
+    function handleResponse(response) {
+        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
+
+    if (loaded) {
+          return (
+            <div className="WeatherForecast">
+                  <div className="row">
+                      {forecast.map(function(dailyForecast, index) {
+                          if (index < 5) {
+                              return (
+                                  <div className="col" key={index}>
+                                    <WeatherForecastDay data={dailyForecast} />
+                                  </div>
+                              );
+                          }
+                      })}                 
                 </div>
             </div>
-        </div>
-    )
+        );     
+
+    } else {
+        let apiKey = "0f09c464cb3936o320tabf9addd38da4";
+        let longitude = props.coordinates.longitude;
+        let latitude = props.coordinates.latitude;
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+    
+        axios.get(apiUrl).then(handleResponse);
+        
+        return null;  
+    }   
 }
